5e93b36be5950497b2a1ecb29fe15c667c6f7359
David Huemer <92david.huemer@gmail.com>
2023-01-26 19:06:00 +0100

    Added FinancifyDataContext tests

---
+++ backend/Financify/Financify.Tests/Dal/Basics/FinancifyDataContextTestBase.cs
@@ -0,0 +1,29 @@
+﻿using Financify.Dal.db;
+using Microsoft.EntityFrameworkCore;
+
+namespace Financify.Tests.Dal.Basics;
+
+public abstract class FinancifyDataContextTestBase : IDisposable
+{
+    protected readonly FinancifyDataContext Context;
+
+    protected FinancifyDataContextTestBase()
+    {
+        var options = new DbContextOptionsBuilder<FinancifyDataContext>()
+            .UseInMemoryDatabase("Financify")
+            .Options;
+
+        Context = new FinancifyDataContext(options);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing) Context.Dispose();
+    }
+}
