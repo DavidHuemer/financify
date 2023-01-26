4b8c701d82b688e3f639195234550e57c664eb89
David Huemer <92david.huemer@gmail.com>
2023-01-26 23:52:29 +0100

    Added PersonCreationDto and UserCreationDto

---
+++ backend/Financify/Financify.Models/Dtos/PersonDtos/UserDtos/UserCreationDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Financify.Models.Dtos.PersonDtos.UserDtos;
+
+/// <summary>
+///     Contains the properties that are required to create a new user.
+/// </summary>
+public record UserCreationDto : PersonCreationDto
+{
+    [Required] public string Password { get; set; } = null!;
+}
