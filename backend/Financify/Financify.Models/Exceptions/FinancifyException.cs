584c5eed418a198717202b9f6295e69b6bbed6db
David Huemer <92david.huemer@gmail.com>
2023-01-31 09:15:01 +0100

    Added EntityNotFound exception

---
+++ backend/Financify/Financify.Models/Exceptions/FinancifyException.cs
@@ -0,0 +1,20 @@
+﻿namespace Financify.Models.Exceptions;
+
+/// <summary>
+///     Base exception for all financify exceptions
+/// </summary>
+public abstract class FinancifyException : Exception
+{
+    protected FinancifyException(string title, string message, string detail)
+    {
+        Title = title;
+        Message = message;
+        Detail = detail;
+    }
+
+    public string Title { get; set; }
+
+    public string Message { get; set; }
+
+    public string Detail { get; set; }
+}
