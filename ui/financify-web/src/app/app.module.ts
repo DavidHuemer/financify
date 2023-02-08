a39e3825b9fc665774ace3d3c21fce8d631591b4
David Huemer <92david.huemer@gmail.com>
2023-02-08 11:30:50 +0100

    Added rat-ui module

--- ui/financify-web/src/app/app.module.ts
+++ ui/financify-web/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RatUiModule} from "./rat-ui/rat-ui.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RatUiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
