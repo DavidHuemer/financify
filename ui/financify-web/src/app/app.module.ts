0352f4b29fdb80a394d4be8fa608724280cd0f3b
David Huemer <92david.huemer@gmail.com>
2023-02-08 20:31:26 +0100

    Added angular-material to the project

--- ui/financify-web/src/app/app.module.ts
+++ ui/financify-web/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {RatUiModule} from "./rat-ui/rat-ui.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {RatUiModule} from "./rat-ui/rat-ui.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RatUiModule
+    RatUiModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
